da7659dc297ca4805d06c83c1c07080f6bf6d15c
Crowdbotics <team@crowdbotics.com>
2020-10-12 21:48:21 +0000

    Update models

--- backend/livwealth_mobile_v__21418/wsgi.py
+++ backend/livwealth_mobile_v__21418/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livwealth_mobile_v__21418.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "livwealth_mobile_v__21418.settings")
 
 application = get_wsgi_application()
